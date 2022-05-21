97c74719f90096efcc45fd6d1aee5d8874263871
woojinv <oh.woojin95@gmail.com>
2022-05-21 18:34:28 -0500

    state variables in createPage

--- src/pages/CreatePage/CreatePage.jsx
+++ src/pages/CreatePage/CreatePage.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeader from "../../components/Header/Header";
 import { Grid } from "semantic-ui-react";
 import { useNavigate } from "react-router-dom";
 
 export default function CreatePage({ user, handleLogout }) {
   const navigate = useNavigate();
+
+  const [error, setError] = useState("");
+  const [state, setState] = useState({
+    title: "",
+    description: "",
+    location: "",
+    date: "",
+  });
+
+  const [selectedFile, setSelectedFile] = useState("");
+
   return (
     <Grid centered>
       <Grid.Row>
