9e3d24e73bffd9816e8b2bbd1a5d53b3e3dcabed
woojinv <oh.woojin95@gmail.com>
2022-05-21 12:34:10 -0500

    name, email, and password inputs on signup form

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -3,7 +3,7 @@ import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 
 import userService from "../../utils/userService";
 import { useNavigate } from "react-router-dom";
-import { Grid, Header, Image } from "semantic-ui-react";
+import { Grid, Header, Image, Form } from "semantic-ui-react";
 
 export default function SignUpPage(props) {
   const [error, setError] = useState("");
@@ -20,6 +20,10 @@ export default function SignUpPage(props) {
       [e.target.name]: e.target.value,
     });
   }
+
+  function handleSubmit() {
+    console.log("handleSubmit function");
+  }
   return (
     <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
       <Grid.Column style={{ maxWidth: 450 }}>
@@ -27,6 +31,33 @@ export default function SignUpPage(props) {
           <Image src="https://storage.snappages.site/3FFMJ4/assets/images/676092_311x310_500.png" />
           Sign Up
         </Header>
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              name="username"
+              placeholder="username"
+              value={state.username}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="password"
+              type="password"
+              placeholder="password"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+          </Segment>
+        </Form>
       </Grid.Column>
     </Grid>
   );
