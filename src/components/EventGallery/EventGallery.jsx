7b917da182f7dc1244eee0dcfffeab7426f32020
woojinv <oh.woojin95@gmail.com>
2022-05-22 18:42:21 -0500

    create map function in EventGallery

--- src/components/EventGallery/EventGallery.jsx
+++ src/components/EventGallery/EventGallery.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import EventCard from "../EventCard/EventCard";
 
-export default function EventGallery(props) {
+export default function EventGallery({ events }) {
   return (
     <div className="EventGallery">
       <h1>This is the Event Gallery</h1>
-      <EventCard />
+      {events.map((event) => {
+        return <EventCard event={event} key={event._id} />;
+      })}
     </div>
   );
 }
