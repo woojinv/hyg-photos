80ee189f28277a1548cc2f9a436aa0911d5c3f17
woojinv <oh.woojin95@gmail.com>
2022-05-21 21:24:35 -0500

    events route

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,2 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const eventsCtrl = require("../../controllers/events");
+const multer = require("multer");
+const upload = multer();
+
+router.post("/", upload.single("photo"), eventsCtrl.create);
