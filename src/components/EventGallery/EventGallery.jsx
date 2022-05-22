124af4fbc76f01bbd7949ce70bf9b2bd33d43221
woojinv <oh.woojin95@gmail.com>
2022-05-22 18:33:46 -0500

    connected eventCard with eventGallery

--- src/components/EventGallery/EventGallery.jsx
+++ src/components/EventGallery/EventGallery.jsx
@@ -2,5 +2,10 @@ import React from "react";
 import EventCard from "../EventCard/EventCard";
 
 export default function EventGallery(props) {
-  return <h1>This is the Event Gallery</h1>;
+  return (
+    <div className="EventGallery">
+      <h1>This is the Event Gallery</h1>
+      <EventCard />
+    </div>
+  );
 }
