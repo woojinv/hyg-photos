e3ad3b0e8e72bbd0780e1dea68683da90858e26b
woojinv <oh.woojin95@gmail.com>
2022-05-21 23:03:16 -0500

    create getAll function in eventApi.js

--- src/utils/eventApi.js
+++ src/utils/eventApi.js
@@ -13,3 +13,14 @@ export async function create(event) {
   if (res.ok) return res.json();
   throw new Error("Bad Credentials! CHECK THE SERVER TERMINAL");
 }
+
+export function getAll() {
+  return fetch(BASE_URL, {
+    headers: {
+      Authorization: "Bearer " + tokenService.getToken(),
+    },
+  }).then((res) => {
+    if (res.ok) return res.json();
+    throw new Error("bad credentials! CHECK THE SERVER TERMINAL");
+  });
+}
