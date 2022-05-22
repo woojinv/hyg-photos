81cb54ffd1465628780810ede0b0c201be849fe2
woojinv <oh.woojin95@gmail.com>
2022-05-21 21:42:24 -0500

    finish create event controller function

--- controllers/events.js
+++ controllers/events.js
@@ -8,6 +8,34 @@ module.exports = {
   create,
 };
 
-function create(req, res) {
+async function create(req, res) {
   console.log("create controller hit");
+  console.log(req.body, req.file, req.user);
+
+  try {
+    const filePath = `${uuidv4()}/${req.file.originalname}`;
+    const params = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: filePath,
+      Body: req.file.buffer,
+    };
+    s3.upload(params, async function (err, data) {
+      console.log(err, "<- from aws");
+      const event = await Event.create({
+        user: req.user,
+        title: req.body.title,
+        description: req.body.description,
+        location: req.body.location,
+        date: req.body.date,
+        photoUrl: data.Location,
+      });
+      console.log(event, "<- this is event");
+      await event.populate("user");
+
+      res.status(201).json({ event: event });
+    });
+  } catch (err) {
+    console.log(err, "<- this is err from events create controller");
+    res.json({ data: err });
+  }
 }
