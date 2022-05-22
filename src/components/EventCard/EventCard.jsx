15e2c13974651166c1ebcb1927d9670d56f6bb3d
woojinv <oh.woojin95@gmail.com>
2022-05-22 18:32:39 -0500

    create eventCard component

---
+++ src/components/EventCard/EventCard.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EventCard(props) {
+  return <h1>This is an Event Card</h1>;
+}
--- src/components/EventGallery/EventGallery.jsx
+++ src/components/EventGallery/EventGallery.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import EventCard from "../EventCard/EventCard";
 
 export default function EventGallery(props) {
   return <h1>This is the Event Gallery</h1>;
