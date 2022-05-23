42e25efdb81360cb112e0eb3c4c8e15c33d57a11
woojinv <oh.woojin95@gmail.com>
2022-05-23 16:03:29 -0500

    event found when uploading photo

--- controllers/photos.js
+++ controllers/photos.js
@@ -1,4 +1,5 @@
 const Photo = require("../models/photo");
+const Event = require("../models/event");
 const S3 = require("aws-sdk/clients/s3");
 const { v4: uuidv4 } = require("uuid");
 
@@ -7,12 +8,10 @@ module.exports = { create };
 
 async function create(req, res) {
   console.log("create controller for photos hit");
-  console.log(
-    req.body,
-    "<- this is req.body",
-    req.params,
-    "<- this is req.params",
-    req.file,
-    "<- this is req.file"
-  );
+  try {
+    const event = await Event.findOne({ title: req.body.eventTitle });
+    console.log(event, "<-this is event");
+  } catch (err) {
+    res.status(400).json({ err });
+  }
 }
