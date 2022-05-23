d83e6128fe3c3c28b4d4b0bbd0270065cd3aa4f0
woojinv <oh.woojin95@gmail.com>
2022-05-23 11:33:08 -0500

    import components into ViewEventPage

--- src/pages/ViewEventPage/ViewEventPage.jsx
+++ src/pages/ViewEventPage/ViewEventPage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import EventInfo from "../../components/EventInfo/EventInfo";
+import PhotoGallery from "../../components/PhotoGallery/PhotoGallery";
+import PhotoCard from "../../components/PhotoCard/PhotoCard";
 
 export default function ViewEventPage({ user, handleLogout }) {
   return (
     <>
       <Header user={user} handleLogout={handleLogout} />
       <h1>This is the View Event Page</h1>;
+      <EventInfo />
+      <PhotoGallery></PhotoGallery>
+      <PhotoCard />
     </>
   );
 }
