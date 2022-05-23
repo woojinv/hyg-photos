278f22136686d5f0a923fef07ee7a38d01b0996d
woojinv <oh.woojin95@gmail.com>
2022-05-22 21:21:31 -0500

    clicking event redirects to ViewEventPage

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -3,5 +3,9 @@ import { Card, Icon, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 export default function EventCard({ event }) {
-  return <Image src={event.photoUrl} />;
+  return (
+    <Link to={`/${event.title}`}>
+      <Image src={event.photoUrl} />
+    </Link>
+  );
 }
