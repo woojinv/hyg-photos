fcc1b3e4d4a660e62d9487fce67ab58ee28ae7b0
woojinv <oh.woojin95@gmail.com>
2022-05-23 12:17:22 -0500

    render EventInfo if event exists

--- src/pages/ViewEventPage/ViewEventPage.jsx
+++ src/pages/ViewEventPage/ViewEventPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Header from "../../components/Header/Header";
+import PageHeader from "../../components/Header/Header";
+import Loading from "../../components/Loader/Loader";
 import EventInfo from "../../components/EventInfo/EventInfo";
 import PhotoGallery from "../../components/PhotoGallery/PhotoGallery";
 
@@ -8,32 +9,54 @@ import * as eventsAPI from "../../utils/eventApi";
 
 export default function ViewEventPage({ user, handleLogout }) {
   const { eventTitle } = useParams();
-
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
   const [event, setEvent] = useState({});
+
   async function getEvent() {
-    console.log("getEvent hit");
-    console.log(eventTitle, "<- this is ttile from getEvent in ViewEventpage");
-    const data = await eventsAPI.getEvent(eventTitle);
-    console.log(data, "<- this is data from getEvent on ViewEventpage");
-    setEvent(data.data);
+    try {
+      console.log("getEvent hit");
+      console.log(
+        eventTitle,
+        "<- this is ttile from getEvent in ViewEventpage"
+      );
+      const data = await eventsAPI.getEvent(eventTitle);
+      console.log(data, "<- this is data from getEvent on ViewEventpage");
+      setEvent(data.data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setError(err.message);
+    }
   }
 
   useEffect(() => {
     getEvent();
   }, []);
 
+  if (loading) {
+    return (
+      <>
+        <PageHeader user={user} handleLogout={handleLogout} />
+        <Loading />
+      </>
+    );
+  }
+
   return (
     <>
-      <Header user={user} handleLogout={handleLogout} />
+      <PageHeader user={user} handleLogout={handleLogout} />
       <h1>This is the View Event Page</h1>;
-      <EventInfo
-        title={event.title}
-        description={event.description}
-        location={event.location}
-        date={event.date}
-        photoUrl={event.photoUrl}
-        id={event._id}
-      />
+      {event ? (
+        <EventInfo
+          title={event.title}
+          description={event.description}
+          location={event.location}
+          date={event.date}
+          photoUrl={event.photoUrl}
+          id={event._id}
+        />
+      ) : null}
       <PhotoGallery />
     </>
   );
