bb93c779c0f26525c1720e6acaa9e5b2a6bfa4b1
woojinv <oh.woojin95@gmail.com>
2022-05-23 12:06:35 -0500

    retrieve event data

--- src/pages/ViewEventPage/ViewEventPage.jsx
+++ src/pages/ViewEventPage/ViewEventPage.jsx
@@ -8,10 +8,14 @@ import * as eventsAPI from "../../utils/eventApi";
 
 export default function ViewEventPage({ user, handleLogout }) {
   const { eventTitle } = useParams();
+
+  const [event, setEvent] = useState({});
   async function getEvent() {
     console.log("getEvent hit");
     console.log(eventTitle, "<- this is ttile from getEvent in ViewEventpage");
     const data = await eventsAPI.getEvent(eventTitle);
+    console.log(data, "<- this is data from getEvent on ViewEventpage");
+    setEvent(data.data);
   }
 
   useEffect(() => {
