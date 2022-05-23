f7ba60ccdcee417a0b3147ae09563e6b9369560b
woojinv <oh.woojin95@gmail.com>
2022-05-22 21:34:12 -0500

    remove header with token auth from eventApi getAll

--- src/utils/eventApi.js
+++ src/utils/eventApi.js
@@ -14,13 +14,8 @@ export async function create(event) {
   throw new Error("Bad Credentials! CHECK THE SERVER TERMINAL");
 }
 
-export function getAll() {
-  return fetch(BASE_URL, {
-    headers: {
-      Authorization: "Bearer " + tokenService.getToken(),
-    },
-  }).then((res) => {
-    if (res.ok) return res.json();
-    throw new Error("bad credentials! CHECK THE SERVER TERMINAL");
-  });
+export async function getAll() {
+  const res = await fetch(BASE_URL);
+  if (res.ok) return res.json();
+  throw new Error("bad credentials! CHECK THE SERVER TERMINAL");
 }
