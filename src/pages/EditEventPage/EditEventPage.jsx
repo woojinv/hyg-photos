cf2421d679d68e0aec48046a2ef615b9a5fbc277
woojinv <oh.woojin95@gmail.com>
2022-05-23 22:27:04 -0500

    declare newEvent variable

--- src/pages/EditEventPage/EditEventPage.jsx
+++ src/pages/EditEventPage/EditEventPage.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeader from "../../components/Header/Header";
 
 export default function EditEventPage({ user, handleLogout, event }) {
   console.log(event, "<- this is event");
+  const [newEvent, setNewEvent] = useState({
+    title: event.title,
+    description: event.description,
+    location: event.location,
+    date: event.date,
+  });
+
   return (
     <>
       <PageHeader user={user} handleLogout={handleLogout} />
