d2a80cbf6c867ce6ed8986af147323aa2f41f99e
woojinv <oh.woojin95@gmail.com>
2022-05-24 14:31:46 -0500

    copy over placesAutoComplete into CreateEventpage

--- src/pages/CreatePage/CreateEventPage.jsx
+++ src/pages/CreatePage/CreateEventPage.jsx
@@ -4,7 +4,11 @@ import PageHeader from "../../components/Header/Header";
 import { Button, Form, Grid, Segment } from "semantic-ui-react";
 import { useNavigate } from "react-router-dom";
 
-import PlacesAutocomplete from "../../components/PlacesAutocomplete/PlacesAutocomplete";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import useOnclickOutside from "react-cool-onclickoutside";
 
 import * as eventsAPI from "../../utils/eventApi";
 
@@ -52,6 +56,62 @@ export default function CreatePage({ user, handleLogout, handleSubmit }) {
     setSelectedFile(e.target.files[0]);
   }
 
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      /* define search scope here */
+    },
+    debounce: 300,
+  });
+  const ref = useOnclickOutside(() => {
+    // When user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+    clearSuggestions();
+  });
+
+  const handleInput = (e) => {
+    // Update the keyword of the input element
+    setValue(e.target.value);
+  };
+
+  const handleSelect =
+    ({ description }) =>
+    () => {
+      // When user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue(description, false);
+      clearSuggestions();
+
+      // Get latitude and longitude via utility functions
+      getGeocode({ address: description }).then((results) => {
+        try {
+          const { lat, lng } = getLatLng(results[0]);
+          console.log("📍 Coordinates: ", { lat, lng });
+        } catch (error) {
+          console.log("😱 Error: ", error);
+        }
+      });
+    };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
   return (
     <Grid centered>
       <Grid.Row>
@@ -87,7 +147,17 @@ export default function CreatePage({ user, handleLogout, handleSubmit }) {
                 required
               /> */}
               <Form.Field>
-                <PlacesAutocomplete />
+                <div ref={ref}>
+                  <input
+                    name="location"
+                    value={value}
+                    onChange={handleInput}
+                    disabled={!ready}
+                    placeholder="Where did this event take place?"
+                  />
+                  {/* We can use the "status" to decide whether we should display the dropdown or not */}
+                  {status === "OK" && <ul>{renderSuggestions()}</ul>}
+                </div>
               </Form.Field>
               <Form.Input
                 type="text"
