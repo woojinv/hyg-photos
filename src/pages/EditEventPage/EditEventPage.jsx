2949d8a5cee60eec25f39d3068abcebc079dd667
woojinv <oh.woojin95@gmail.com>
2022-05-24 18:23:32 -0500

    autocomplete works on edit event page

--- src/pages/EditEventPage/EditEventPage.jsx
+++ src/pages/EditEventPage/EditEventPage.jsx
@@ -5,6 +5,12 @@ import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import PageHeader from "../../components/Header/Header";
 import { useNavigate } from "react-router-dom";
 
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import useOnclickOutside from "react-cool-onclickoutside";
+
 import * as eventAPI from "../../utils/eventApi";
 
 export default function EditEventPage({ user, handleLogout, event }) {
@@ -40,7 +46,70 @@ export default function EditEventPage({ user, handleLogout, event }) {
       ...newEvent,
       [e.target.name]: e.target.value,
     });
+    const inputField = e.target.attributes[0].value;
+
+    if (inputField === "location") {
+      handleInput(e);
+    }
   }
+
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      /* define search scope here */
+    },
+    debounce: 300,
+  });
+  const ref = useOnclickOutside(() => {
+    // When user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+    clearSuggestions();
+  });
+
+  const handleInput = (e) => {
+    // Update the keyword of the input element
+    setValue(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleSelect =
+    ({ description }) =>
+    () => {
+      // When user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue(description, false);
+      clearSuggestions();
+
+      // Get latitude and longitude via utility functions
+      getGeocode({ address: description }).then((results) => {
+        try {
+          const { lat, lng } = getLatLng(results[0]);
+
+          console.log("📍 Coordinates: ", { lat, lng });
+        } catch (error) {
+          console.log("😱 Error: ", error);
+        }
+      });
+    };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
 
   return (
     <Grid centered>
@@ -69,14 +138,19 @@ export default function EditEventPage({ user, handleLogout, event }) {
                 onChange={handleChange}
                 required
               />
-              <Form.Input
-                type="text"
-                name="location"
-                placeholder={newEvent.location}
-                value={newEvent.location}
-                onChange={handleChange}
-                required
-              />
+              <Form.Field>
+                <div ref={ref}>
+                  <input
+                    name="location"
+                    value={value}
+                    onChange={handleChange}
+                    disabled={!ready}
+                    placeholder="Where did this event take place?"
+                  />
+                  {/* We can use the "status" to decide whether we should display the dropdown or not */}
+                  {status === "OK" && <ul>{renderSuggestions()}</ul>}
+                </div>
+              </Form.Field>
               <Form.Input
                 type="text"
                 name="date"
