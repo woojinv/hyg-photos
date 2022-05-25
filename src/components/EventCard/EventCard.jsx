256c0f213ce30ff4be6d5cc54a7d8573c59fd935
woojinv <oh.woojin95@gmail.com>
2022-05-25 18:27:47 -0500

    event card is a link

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { Card, Icon, Image } from "semantic-ui-react";
+import { Icon, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+
+// bootstrap
+import Card from "react-bootstrap/Card";
 
 export default function EventCard({ event, user, deleteEvent }) {
   function handleClick(e) {
@@ -10,17 +13,27 @@ export default function EventCard({ event, user, deleteEvent }) {
 
   if (user) {
     return (
-      <Card>
-        <Link to={`/${event.title}`}>
-          <Image src={event.photoUrl} />
-          <Card.Content>
-            <Card.Description>{event.title}</Card.Description>
-          </Card.Content>
+      <Card style={{ width: "18rem" }}>
+        <Link className="card-link" to={`/${event.title}`}>
+          <Card.Img variant="top" src={event.photoUrl} />
+          <Card.Body>
+            <Card.Title>{event.title}</Card.Title>
+            <Card.Text>{event.description}</Card.Text>
+          </Card.Body>
         </Link>
-        {event.user._id === user._id ? (
-          <Icon name="delete" onClick={handleClick} eventid={event._id} />
-        ) : null}
       </Card>
+
+      // <Card>
+      //   <Link to={`/${event.title}`}>
+      //     <Image src={event.photoUrl} />
+      //     <Card.Content>
+      //       <Card.Description>{event.title}</Card.Description>
+      //     </Card.Content>
+      //   </Link>
+      //   {event.user._id === user._id ? (
+      //     <Icon name="delete" onClick={handleClick} eventid={event._id} />
+      //   ) : null}
+      // </Card>
     );
   }
 
