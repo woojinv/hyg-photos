91018adef8b16ee80b125734272206c328587475
woojinv <oh.woojin95@gmail.com>
2022-05-25 17:52:21 -0500

    logo on sign up page is a link

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 
 import userService from "../../utils/userService";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { Grid, Header, Image, Form, Segment, Button } from "semantic-ui-react";
 
 export default function SignUpPage(props) {
@@ -42,7 +42,12 @@ export default function SignUpPage(props) {
     <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
       <Grid.Column style={{ maxWidth: 450 }}>
         <Header as="h2" color="teal" textAlign="center">
-          <Image src="https://storage.snappages.site/3FFMJ4/assets/images/676092_311x310_500.png" />
+          <Link to="/">
+            <img
+              style={{ width: "75px" }}
+              src="https://storage.snappages.site/3FFMJ4/assets/images/676092_311x310_500.png"
+            />
+          </Link>{" "}
           Sign Up
         </Header>
         <Form autoComplete="off" onSubmit={handleSubmit}>
