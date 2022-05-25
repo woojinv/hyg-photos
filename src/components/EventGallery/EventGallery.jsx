4a0789e14a3bd69de60940a6f93095908bcc4e96
woojinv <oh.woojin95@gmail.com>
2022-05-25 18:15:29 -0500

    refactor eventGallery with bootstrap

--- src/components/EventGallery/EventGallery.jsx
+++ src/components/EventGallery/EventGallery.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import EventCard from "../EventCard/EventCard";
 import { Grid, Image } from "semantic-ui-react";
 
+// React bootstrap
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 export default function EventGallery({ events, user, deleteEvent }) {
   return (
-    <Grid>
-      {events.map((event, i) => {
-        return (
-          <EventCard
-            event={event}
-            key={i}
-            user={user}
-            deleteEvent={deleteEvent}
-          />
-        );
-      })}
-    </Grid>
+    <Container>
+      <Row>
+        {events.map((event, i) => {
+          return (
+            <Col>
+              <EventCard
+                event={event}
+                key={i}
+                user={user}
+                deleteEvent={deleteEvent}
+              />
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
   );
 }
