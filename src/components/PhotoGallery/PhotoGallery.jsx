2a4d857a4a56daa97a99c727c86087d4425554dc
woojinv <oh.woojin95@gmail.com>
2022-05-25 22:35:10 -0500

    bootstrap to PhotoGallery

--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import PhotoCard from "../PhotoCard/PhotoCard";
 
+// bootstrap
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 export default function PhotoGallery({ photos, user, deletePhoto }) {
   return (
-    <>
-      <h1>This is the PhotoGallery Component</h1>
-      {photos.map((photo, i) => {
-        return (
-          <PhotoCard
-            photo={photo}
-            key={i}
-            user={user}
-            deletePhoto={deletePhoto}
-          />
-        );
-      })}
-    </>
+    <Container>
+      <Row>
+        {photos.map((photo, i) => {
+          return (
+            <Col className="photos-column">
+              <PhotoCard
+                photo={photo}
+                key={i}
+                user={user}
+                deletePhoto={deletePhoto}
+              />
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
   );
 }
