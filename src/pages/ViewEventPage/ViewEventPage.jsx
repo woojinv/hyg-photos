e23c2c31f1cfa32b306fa62655e19649025ab310
woojinv <oh.woojin95@gmail.com>
2022-05-25 19:22:28 -0500

    work on event info container

--- src/pages/ViewEventPage/ViewEventPage.jsx
+++ src/pages/ViewEventPage/ViewEventPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
-import { Icon } from "semantic-ui-react";
 import Map from "../../components/Map/Map";
 import PageHeader from "../../components/Header/Header";
 import Loading from "../../components/Loader/Loader";
