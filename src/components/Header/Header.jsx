f6974ba1c7cf27251acc3463f2325b2dd2046f2b
woojinv <oh.woojin95@gmail.com>
2022-05-27 11:07:26 -0500

    addPhotoForm

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+// Bootstrap
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 
