8cd6cf201adbe038c65d92db6f549bf319912bd1
woojinv <oh.woojin95@gmail.com>
2022-05-26 22:42:37 -0500

    event title must be unique

--- models/event.js
+++ models/event.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const eventSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    title: String,
+    title: { type: String, unique: true },
     description: String,
     location: String,
     date: { type: String, default: Date },
