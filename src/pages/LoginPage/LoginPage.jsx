c13d5c99026c0854cf417028e5f4bfe13bdb8a07
woojinv <oh.woojin95@gmail.com>
2022-06-09 17:05:33 -0500

    animate login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
+// framer-motion
+import { motion } from "framer-motion";
+
 // Components
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import PageHeader from "../../components/Header/Header";
@@ -80,50 +83,71 @@ export default function LoginPage({ user, handleLogout, handleSignUpOrLogin }) {
         verticalAlign="middle"
       >
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" color="teal" textAlign="center">
-            <Link to="/">
-              <img
-                style={{ width: "75px" }}
-                src="https://storage.snappages.site/3FFMJ4/assets/images/676092_311x310_500.png"
-              />
-            </Link>{" "}
-            Log-in to your account
-          </Header>
-          <Form autoComplete="off" onSubmit={handleSubmit}>
-            <Segment stacked>
-              <Form.Input
-                type="email"
-                name="email"
-                placeholder="Email"
-                label="Email"
-                value={state.email}
-                onChange={handleChange}
-                required
-              />
-              <Form.Input
-                type="password"
-                name="password"
-                placeholder="Password"
-                label="Password"
-                value={state.password}
-                onChange={handleChange}
-                required
-              />
-              <Button
-                color="teal"
-                fluid
-                size="large"
-                type="submit"
-                className="btn"
-              >
-                Login
-              </Button>
-            </Segment>
-          </Form>
-          <Message style={{ textAlign: "center" }}>
-            New to us? <Link to="/signup">Sign Up</Link>
-          </Message>
-          {error ? <ErrorMessage error={error} /> : null}
+          <motion.div
+            style={{ paddingBottom: "15px" }}
+            initial={{ scale: 0, x: 500, y: -500 }}
+            animate={{ scale: 1, x: 0, y: 0 }}
+            transition={{ ease: "easeInOut", duration: 0.5 }}
+          >
+            <Header as="h2" color="teal" textAlign="center">
+              <Link to="/">
+                <img
+                  style={{ width: "75px" }}
+                  src="https://storage.snappages.site/3FFMJ4/assets/images/676092_311x310_500.png"
+                />
+              </Link>{" "}
+              Log-in to your account
+            </Header>
+          </motion.div>
+
+          <motion.div
+            initial={{ scale: 0, x: 500, y: -500 }}
+            animate={{ scale: 1, x: 0, y: 0 }}
+            transition={{ delay: 0.1, ease: "easeInOut", duration: 0.5 }}
+          >
+            <Form autoComplete="off" onSubmit={handleSubmit}>
+              <Segment stacked>
+                <Form.Input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  label="Email"
+                  value={state.email}
+                  onChange={handleChange}
+                  required
+                />
+                <Form.Input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  label="Password"
+                  value={state.password}
+                  onChange={handleChange}
+                  required
+                />
+                <Button
+                  color="teal"
+                  fluid
+                  size="large"
+                  type="submit"
+                  className="btn"
+                >
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+          </motion.div>
+          <motion.div
+            style={{ marginTop: "15px" }}
+            initial={{ scale: 0, x: 500, y: -500 }}
+            animate={{ scale: 1, x: 0, y: 0 }}
+            transition={{ delay: 0.2, ease: "easeInOut", duration: 0.5 }}
+          >
+            <Message style={{ textAlign: "center" }}>
+              New to us? <Link to="/signup">Sign Up</Link>
+            </Message>
+            {error ? <ErrorMessage error={error} /> : null}
+          </motion.div>
         </Grid.Column>
       </Grid>
     </>
