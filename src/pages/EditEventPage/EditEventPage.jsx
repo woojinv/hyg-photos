2a9e1dea89391d390b3cc4d88c3acd89b09b3dba
woojinv <oh.woojin95@gmail.com>
2022-06-09 17:15:29 -0500

    animate edit event form

--- src/pages/EditEventPage/EditEventPage.jsx
+++ src/pages/EditEventPage/EditEventPage.jsx
@@ -153,76 +153,89 @@ export default function EditEventPage({ user, handleLogout, event }) {
   return (
     <Grid centered style={{ height: "100vh" }} verticalAlign="middle">
       <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as="h2" color="teal" textAlign="center">
-          <Link to="/">
-            <img
-              style={{ width: "75px" }}
-              src="https://storage.snappages.site/3FFMJ4/assets/images/676092_311x310_500.png"
-            />
-          </Link>{" "}
-          Edit this Event
-        </Header>{" "}
-        <Form
-          style={{ maxWidth: 450 }}
-          autoComplete="off"
-          onSubmit={handleSubmit}
+        <motion.div
+          style={{ paddingBottom: "15px" }}
+          initial={{ scale: 0, x: 500, y: -500 }}
+          animate={{ scale: 1, x: 0, y: 0 }}
+          transition={{ ease: "easeInOut", duration: 0.5 }}
         >
-          <Segment stacked>
-            <Form.Input
-              type="text"
-              name="title"
-              label="Title"
-              placeholder={newEvent.title}
-              value={newEvent.title}
-              onChange={handleChange}
-              required
-            />
-            <Form.TextArea
-              type="text"
-              name="description"
-              label="Description"
-              placeholder={newEvent.description}
-              value={newEvent.description}
-              onChange={handleChange}
-              required
-            />
-            <Form.Field>
-              <div ref={ref}>
-                <label>
-                  <strong>Location</strong>
-                </label>
-                <input
-                  name="location"
-                  value={value}
-                  onChange={handleChange}
-                  disabled={!ready}
-                  placeholder={newEvent.location}
-                />
-                {/* We can use the "status" to decide whether we should display the dropdown or not */}
-                {status === "OK" && <ul>{renderSuggestions()}</ul>}
-              </div>
-            </Form.Field>
-            <Form.Input
-              type="date"
-              name="date"
-              label="Date"
-              placeholder={newEvent.date}
-              value={newEvent.date}
-              onChange={handleChange}
-              required
-            />
-            <Button
-              color="teal"
-              fluid
-              size="large"
-              type="submit"
-              className="btn"
-            >
-              Submit
-            </Button>
-          </Segment>
-          {error ? <ErrorMessage error={error} /> : null}
-        </Form>
+          <Header as="h2" color="teal" textAlign="center">
+            <Link to="/">
+              <img
+                style={{ width: "75px" }}
+                src="https://storage.snappages.site/3FFMJ4/assets/images/676092_311x310_500.png"
+              />
+            </Link>{" "}
+            Edit this Event
+          </Header>{" "}
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0, x: 500, y: -500 }}
+          animate={{ scale: 1, x: 0, y: 0 }}
+          transition={{ delay: 0.1, ease: "easeInOut", duration: 0.5 }}
+        >
+          <Form
+            style={{ maxWidth: 450 }}
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
+            <Segment stacked>
+              <Form.Input
+                type="text"
+                name="title"
+                label="Title"
+                placeholder={newEvent.title}
+                value={newEvent.title}
+                onChange={handleChange}
+                required
+              />
+              <Form.TextArea
+                type="text"
+                name="description"
+                label="Description"
+                placeholder={newEvent.description}
+                value={newEvent.description}
+                onChange={handleChange}
+                required
+              />
+              <Form.Field>
+                <div ref={ref}>
+                  <label>
+                    <strong>Location</strong>
+                  </label>
+                  <input
+                    name="location"
+                    value={value}
+                    onChange={handleChange}
+                    disabled={!ready}
+                    placeholder={newEvent.location}
+                  />
+                  {/* We can use the "status" to decide whether we should display the dropdown or not */}
+                  {status === "OK" && <ul>{renderSuggestions()}</ul>}
+                </div>
+              </Form.Field>
+              <Form.Input
+                type="date"
+                name="date"
+                label="Date"
+                placeholder={newEvent.date}
+                value={newEvent.date}
+                onChange={handleChange}
+                required
+              />
+              <Button
+                color="teal"
+                fluid
+                size="large"
+                type="submit"
+                className="btn"
+              >
+                Submit
+              </Button>
+            </Segment>
+            {error ? <ErrorMessage error={error} /> : null}
+          </Form>
+        </motion.div>
       </Grid.Column>
     </Grid>
   );
