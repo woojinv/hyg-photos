a1a71c1e17928f982c5bb3959219fa211baa24ef
woojinv <oh.woojin95@gmail.com>
2022-06-09 21:58:06 -0500

    animation to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// framer-motion
+import { motion } from "framer-motion";
+
 // Bootstrap
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -10,43 +13,54 @@ export default function Footer() {
     <>
       <Navbar bg="light" variant="light" expand="lg">
         <Container>
-          <Navbar.Brand href="http://hebronem.org/youth" target="_blank">
-            Hebron EM - Youth
-          </Navbar.Brand>
-
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <Navbar.Brand href="http://hebronem.org/youth" target="_blank">
+              Hebron EM - Youth
+            </Navbar.Brand>
+          </motion.div>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link
-                href="https://linktr.ee/HebronYouthGroup"
-                target="_blank"
-              >
-                Linktree
-              </Nav.Link>
-              <Nav.Link
-                href="https://www.instagram.com/hebron.yg/"
-                target="_blank"
-              >
-                Instagram
-              </Nav.Link>
-              <Nav.Link
-                href="https://discord.com/invite/2UAYCCPFW8"
-                target="_blank"
-              >
-                Discord
-              </Nav.Link>
-              <Nav.Link href="mailto:hebronacts@gmail.com" target="_blank">
-                Email
-              </Nav.Link>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Nav.Link
+                  href="https://linktr.ee/HebronYouthGroup"
+                  target="_blank"
+                >
+                  Linktree
+                </Nav.Link>
+              </motion.div>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Nav.Link
+                  href="https://www.instagram.com/hebron.yg/"
+                  target="_blank"
+                >
+                  Instagram
+                </Nav.Link>
+              </motion.div>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Nav.Link
+                  href="https://discord.com/invite/2UAYCCPFW8"
+                  target="_blank"
+                >
+                  Discord
+                </Nav.Link>
+              </motion.div>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Nav.Link href="mailto:hebronacts@gmail.com" target="_blank">
+                  Email
+                </Nav.Link>
+              </motion.div>
             </Nav>
           </Navbar.Collapse>
           <Nav>
-            <Nav.Link
-              href="https://www.linkedin.com/in/woojin-oh/"
-              target="_blank"
-            >
-              Created by Woojin Oh
-            </Nav.Link>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <Nav.Link
+                href="https://www.linkedin.com/in/woojin-oh/"
+                target="_blank"
+              >
+                Created by Woojin Oh
+              </Nav.Link>
+            </motion.div>
           </Nav>
         </Container>
       </Navbar>
