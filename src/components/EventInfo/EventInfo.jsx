194eba696d12fc27ff60119546eb2af5fa1b8592
woojinv <oh.woojin95@gmail.com>
2022-06-09 22:04:12 -0500

    animation to cover photo

--- src/components/EventInfo/EventInfo.jsx
+++ src/components/EventInfo/EventInfo.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import PhotoModal from "../PhotoModal/PhotoModal";
 
+// framer-motion
+import { motion } from "framer-motion";
+
 // CSS
 import "../../pages/ViewEventPage/ViewEventPage.css";
 
@@ -18,28 +21,30 @@ export default function EventInfo({
   const [modalShow, setModalShow] = useState(false);
   return (
     <>
-      <Card
-        className="bg-dark text-white shadow-2-strong"
-        onClick={() => setModalShow(true)}
-      >
-        <Card.Img
-          src={photoUrl}
-          alt={"Picture of" + title}
-          style={{ opacity: 0.2, maxHeight: 400, objectFit: "cover" }}
-        />
-        <Card.ImgOverlay
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-end",
-            cursor: "pointer",
-          }}
+      {" "}
+      <motion.div whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.98 }}>
+        <Card
+          className="bg-dark text-white shadow-2-strong"
+          onClick={() => setModalShow(true)}
         >
-          <Card.Title className="event-info-date">{date}</Card.Title>
-          <Card.Text>{description}</Card.Text>
-        </Card.ImgOverlay>
-      </Card>
-
+          <Card.Img
+            src={photoUrl}
+            alt={"Picture of" + title}
+            style={{ opacity: 0.2, maxHeight: 400, objectFit: "cover" }}
+          />
+          <Card.ImgOverlay
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-end",
+              cursor: "pointer",
+            }}
+          >
+            <Card.Title className="event-info-date">{date}</Card.Title>
+            <Card.Text>{description}</Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+      </motion.div>
       <PhotoModal
         show={modalShow}
         photo={photoUrl}
